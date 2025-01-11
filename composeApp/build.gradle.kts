3278dd4ae0cdf8dda9658722a266584929c4f171
Jose-Alberto-Rodrigues-Neto <allio.professional@gmail.com>
2025-01-11 16:57:12 -0300

    fix; removing .exe

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -112,7 +112,7 @@ compose.desktop {
         mainClass = "travel.planning.MainKt"
 
         nativeDistributions {
-            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb, TargetFormat.Exe)
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "travel.planning"
             packageVersion = "1.0.0"
         }

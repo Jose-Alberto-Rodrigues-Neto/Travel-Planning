b4ecafc78ceb07efd89b20e5f6dc3bd6c10421a3
Jose-Alberto-Rodrigues-Neto <allio.professional@gmail.com>
2025-01-14 11:55:32 -0300

    feat: creating folder to preview components

---
+++ composeApp/src/androidMain/kotlin/travel/planning/previews/EntryScreenPreview.kt
@@ -0,0 +1,11 @@
+package travel.planning.previews
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import travel.planning.EntryScreen
+
+@Preview
+@Composable
+fun PreviewScree(){
+    EntryScreen()
+}

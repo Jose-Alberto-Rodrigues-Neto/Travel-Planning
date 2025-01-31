1b5f80843b6ad5d44722f11ae27b00eaef5f50cb
Alberto Rodrigues <allio.professional@gmail.com>
2025-01-30 21:35:03 -0300

    feat: creating login page

--- composeApp/src/commonMain/kotlin/travel/planning/EntryScreen.kt
+++ composeApp/src/commonMain/kotlin/travel/planning/EntryScreen.kt
@@ -5,9 +5,11 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material.Button
@@ -15,9 +17,15 @@ import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import travelplanning.composeapp.generated.resources.LoginBackground
@@ -34,35 +42,55 @@ fun EntryScreen(){
     ){
         Image(
             modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop,
             painter = painterResource(Res.drawable.LoginBackground),
             contentDescription = "Login Banner"
         )
         Column(
-            modifier = Modifier.fillMaxHeight().padding(vertical = 15.dp),
-            verticalArrangement = Arrangement.Bottom
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(bottom = 24.dp, start = 16.dp, end = 16.dp, top = 150.dp),
+            verticalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.CenterHorizontally
         ){
-            DefaultButton(
-                modifier = Modifier.fillMaxWidth().padding(10.dp),
-                title = "Login",
-                onClick = {
-                    println("Login")
-                }
-            )
-            DefaultButton(
-                modifier = Modifier.fillMaxWidth().padding(10.dp),
-                title = "Sign Up",
-                onClick = { }
-            )
-            TextButton(
-                modifier = Modifier.padding(20.dp),
-                onClick = {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Text(
+                    text = "Travel Planning",
+                    fontSize = 42.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color(0xFF1D1D1D),
+                    fontFamily = FontFamily.Serif
+                )
 
-                },
-                colors = ButtonDefaults.textButtonColors(contentColor = Color.Red)
-            ){
-                Text("Ainda não possui conta? Faça o cadastro")
+                Text(
+                    modifier = Modifier.padding(vertical = 20.dp),
+                    text = "Organize, planeje e viva sua melhor viagem.",
+                    fontSize = 25.sp,
+                    fontWeight = FontWeight.Medium,
+                    color = Color(0xFF2F2F2F),
+                    textAlign = TextAlign.Center
+                )
             }
+
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+            ){
+                DefaultButton(
+                    modifier = Modifier.fillMaxWidth().padding(5.dp),
+                    title = "Login",
+                    onClick = {
+                        println("Login")
+                    }
+                )
+                DefaultButton(
+                    modifier = Modifier.fillMaxWidth().padding(5.dp),
+                    title = "Sign Up",
+                    onClick = { }
+                )
         }
+
+        }
+
     }
 
 
--- composeApp/src/desktopMain/kotlin/travel/planning/main.kt
+++ composeApp/src/desktopMain/kotlin/travel/planning/main.kt
@@ -8,6 +8,6 @@ fun main() = application {
         onCloseRequest = ::exitApplication,
         title = "Travel Planning",
     ) {
-        App()
+        EntryScreen()
     }
 }
